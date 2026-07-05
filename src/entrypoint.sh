05b0ba0d12e8dceed9726e6e2553e067c3f1411b
Enoch Nartey <yawenochnartey@gmail.com>
2026-07-05 21:01:27 +0200

    update: entrypoint updates

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+python -c "from database import engine; from models import Base; Base.metadata.create_all(bind=engine)"
 python seed.py
 python crawler.py
 uvicorn main:app --host 0.0.0.0 --port 8000
